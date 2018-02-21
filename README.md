c24a6c990109ccce05854caaea5b99542658f840
Hidetake Iwata <int128@gmail.com>
2018-02-21 11:38:12 +0900

    Add script

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # helm-github-pages
-Publish your Helm Charts on GitHub Pages
+
+Publish your Helm Charts on GitHub Pages using CircleCI.
+
+## Getting Started
+
+Create `.circleci/config.yml` with the following content:
+
+```yaml
+version: 2
+jobs:
+  build:
+    docker:
+      - image: alpine
+    steps:
+      - checkout
+      - run:
+          name: helm-github-pages
+          command: wget -O - https://raw.githubusercontent.com/int128/helm-github-pages/master/publish.sh | sh
+```
+
+Your repository should have the following directories:
+
+- Your Repository
+  - `charts/`
+    - `awesome_chart/`
+      - `Chart.yaml`
+      - ...
+    - `great_chart/`
+      - `Chart.yaml`
+      - ...
+    - ...
+
+If the master branch is pushed, syntax checking and publishing are performed.
+Otherwise, only syntax checking is performed.
+
+### Configuration
+
+You can set the following environment variables:
+
+- `HELM_VERSION` = Helm version (defaults to 2.8.1)
+- `HELM_CHARTS_SOURCE` = Helm Charts source directory (defaults to `./charts`)
+
+## Contribution
+
+This is an open source software licensed under Apache License 2.0.
+Feel free to open issues or pull requests.
+
---
+++ publish.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+set -e
+set -o pipefail
+
+[ -z "$HELM_VERSION" ] && HELM_VERSION=2.8.1
+echo "HELM_VERSION=$HELM_VERSION"
+
+[ -z "$HELM_CHARTS_SOURCE" ] && HELM_CHARTS_SOURCE="$PWD/charts"
+echo "HELM_CHARTS_SOURCE=$HELM_CHARTS_SOURCE"
+[ -d "$HELM_CHARTS_SOURCE" ] || {
+  echo "ERROR: Could not find Helm charts in $HELM_CHARTS_SOURCE"
+  exit 1
+}
+
+echo "CIRCLE_BRANCH=$CIRCLE_BRANCH"
+[ "$CIRCLE_BRANCH" ] || {
+  echo "ERROR: Could not determine the current branch"
+  exit 1
+}
+
+echo '>> Prepare...'
+mkdir -p /tmp/helm/bin
+mkdir -p /tmp/helm/publish
+apk update
+apk add ca-certificates git openssh
+
+echo '>> Installing Helm...'
+cd /tmp/helm/bin
+wget "https://storage.googleapis.com/kubernetes-helm/helm-v${HELM_VERSION}-linux-amd64.tar.gz"
+tar -zxf "helm-v${HELM_VERSION}-linux-amd64.tar.gz"
+chmod +x linux-amd64/helm
+HELM="$PWD/linux-amd64/helm"
+"$HELM" version -c
+"$HELM" init -c
+
+echo ">> Check out gh-pages branch from $CIRCLE_REPOSITORY_URL..."
+cd /tmp/helm/publish
+mkdir -p "$HOME/.ssh"
+ssh-keyscan -H github.com >> "$HOME/.ssh/known_hosts"
+git clone -b gh-pages "$CIRCLE_REPOSITORY_URL" . || {
+  git init
+  git checkout -b gh-pages
+  git remote add origin "$CIRCLE_REPOSITORY_URL"
+}
+
+echo '>> Building charts...'
+find "$HELM_CHARTS_SOURCE" -mindepth 1 -maxdepth 1 -type d | while read chart; do
+  echo ">>> helm lint $chart"
+  "$HELM" lint "$chart"
+  echo ">>> helm package $chart"
+  "$HELM" package "$chart"
+done
+echo '>>> helm repo index'
+"$HELM" repo index .
+
+if [ "$CIRCLE_BRANCH" != "master" ]; then
+  echo "Current branch is not master and do not publish"
+  exit 0
+fi
+
+echo ">> Publishing to $CIRCLE_REPOSITORY_URL"
+git config user.email "$CIRCLE_USERNAME@users.noreply.github.com"
+git config user.name CircleCI
+git add .
+git status
+git commit -m "Published by CircleCI $CIRCLE_BUILD_URL"
+git push origin gh-pages
