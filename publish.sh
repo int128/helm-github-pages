c13f6cd518c4b6c3c9449eb8c1289bb606394137
Hidetake Iwata <int128@gmail.com>
2018-03-05 10:42:31 +0900

    Publish to external repository

--- publish.sh
+++ publish.sh
@@ -4,21 +4,24 @@ set -o pipefail
 
 WORKING_DIRECTORY="$PWD"
 
-[ -z "$HELM_VERSION" ] && HELM_VERSION=2.8.1
-echo "HELM_VERSION=$HELM_VERSION"
-
+[ -z "$GITHUB_PAGES_REPO" ] && GITHUB_PAGES_REPO="$CIRCLE_REPOSITORY_URL"
+[ -z "$GITHUB_PAGES_BRANCH" ] && GITHUB_PAGES_BRANCH=gh-pages
 [ -z "$HELM_CHARTS_SOURCE" ] && HELM_CHARTS_SOURCE="$WORKING_DIRECTORY/charts"
-echo "HELM_CHARTS_SOURCE=$HELM_CHARTS_SOURCE"
 [ -d "$HELM_CHARTS_SOURCE" ] || {
   echo "ERROR: Could not find Helm charts in $HELM_CHARTS_SOURCE"
   exit 1
 }
-
-echo "CIRCLE_BRANCH=$CIRCLE_BRANCH"
+[ -z "$HELM_VERSION" ] && HELM_VERSION=2.8.1
 [ "$CIRCLE_BRANCH" ] || {
   echo "ERROR: Could not determine the current branch"
   exit 1
 }
+
+echo "GITHUB_PAGES_REPO=$GITHUB_PAGES_REPO"
+echo "GITHUB_PAGES_BRANCH=$GITHUB_PAGES_BRANCH"
+echo "HELM_CHARTS_SOURCE=$HELM_CHARTS_SOURCE"
+echo "HELM_VERSION=$HELM_VERSION"
+echo "CIRCLE_BRANCH=$CIRCLE_BRANCH"
 
 echo '>> Prepare...'
 mkdir -p /tmp/helm/bin
@@ -35,22 +38,20 @@ HELM="$PWD/linux-amd64/helm"
 "$HELM" version -c
 "$HELM" init -c
 
-echo ">> Check out gh-pages branch from $CIRCLE_REPOSITORY_URL..."
+echo ">> Checking out $GITHUB_PAGES_BRANCH branch from $GITHUB_PAGES_REPO"
 cd /tmp/helm/publish
 mkdir -p "$HOME/.ssh"
 ssh-keyscan -H github.com >> "$HOME/.ssh/known_hosts"
-git clone -b gh-pages "$CIRCLE_REPOSITORY_URL" . || {
-  git init
-  git checkout -b gh-pages
-  git remote add origin "$CIRCLE_REPOSITORY_URL"
-}
+git clone -b "$GITHUB_PAGES_BRANCH" "$GITHUB_PAGES_REPO" .
 
 echo '>> Building charts...'
 find "$HELM_CHARTS_SOURCE" -mindepth 1 -maxdepth 1 -type d | while read chart; do
   echo ">>> helm lint $chart"
   "$HELM" lint "$chart"
-  echo ">>> helm package $chart"
-  "$HELM" package "$chart"
+  chart_name="`basename "$chart"`"
+  echo ">>> helm package -d $chart_name $chart"
+  mkdir -p "$chart_name"
+  "$HELM" package -d "$chart_name" "$chart"
 done
 echo '>>> helm repo index'
 "$HELM" repo index .
@@ -60,14 +61,10 @@ if [ "$CIRCLE_BRANCH" != "master" ]; then
   exit 0
 fi
 
-echo ">> Copying .circleci/config.yml to prevent building gh-pages branch"
-mkdir -p .circleci/
-cp -a "$WORKING_DIRECTORY/.circleci/config.yml" .circleci/config.yml
-
-echo ">> Publishing to $CIRCLE_REPOSITORY_URL"
+echo ">> Publishing to $GITHUB_PAGES_BRANCH branch of $GITHUB_PAGES_REPO"
 git config user.email "$CIRCLE_USERNAME@users.noreply.github.com"
 git config user.name CircleCI
 git add .
 git status
 git commit -m "Published by CircleCI $CIRCLE_BUILD_URL"
-git push origin gh-pages
+git push origin "$GITHUB_PAGES_BRANCH"
